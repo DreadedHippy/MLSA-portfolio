7b07624b69b3cf8f809e67322d4aa7cf5720e387
Aizon <onotaizee@gmail.com>
2023-09-02 22:23:43 +0000

    added education section and more styling

---
+++ src/Components/Education.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <section className="light" id="education">
+      <h2>Education</h2>
+      <div className="education">
+        <div className="education__item">
+          <h3 className="education__item__title">University of Lagos</h3>
+          <p className="education__item__description">BSc. Computer Science</p>
+          <p className="education__item__description">2021 - 2025</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Education;
